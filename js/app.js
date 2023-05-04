7b668890909143db012f1156ba7bc578cdaed8d4
Igor Voitov <igorgrime@gmail.com>
2023-05-04 18:38:31 +0300

    add burger js

--- js/app.js
+++ js/app.js
@@ -56,8 +56,27 @@ function closeModal(currentModal) {
 
 const burger = document.getElementById('sidebarToggle');
 const sidebar = document.getElementById('sidebar');
+const page = document.getElementById('page');
 
 burger.addEventListener('click', event => {
-    document.body.classList.toggle('show-sidebar');
-    document.body.classList.toggle('no-scroll');
-})
+    if (document.body.classList.contains('show-sidebar')) {
+        closeSidebar()
+    } else {
+        showSidebar()
+    }
+});
+
+function showSidebar() {
+    let mask = document.createElement('div');
+    mask.classList.add('page__mask');
+    mask.addEventListener('click', closeSidebar);
+    page.appendChild(mask);
+    body.classList.add('show-sidebar');
+    body.classList.add('no-scroll');
+};
+
+function closeSidebar() {
+    body.classList.remove('show-sidebar');
+    body.classList.remove('no-scroll');
+    document.querySelector('.page__mask').remove();
+}
